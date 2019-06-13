710cc9609e3d6c2f66ae32db4a18e9f3a691234d
Stas Shapinskiy <stanfitt93@gmail.com>
2019-06-13 10:44:13 +0500

    smth

--- banners/models.py
+++ banners/models.py
@@ -2,11 +2,16 @@ from django.db import models
 from django.utils.safestring import mark_safe
 from django.utils.timezone import now
 
+class BannerManager(models.Manager):
+    def all(self,*args,**kwargs):
+        return super(BannerManager,self).get_queryset().filter(is_active=True)
+
 
 class Banner(models.Model):
     title = models.CharField(max_length=200)
     is_active = models.BooleanField(default=False)
     ban_img = models.ImageField(upload_to='banners/images',null=True,blank=True)
+    objects = BannerManager()
 
     class Meta:
         verbose_name = 'Баннер'
